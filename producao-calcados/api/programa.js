56d6a27f8ef25be184bfd45bbeed1db0d134d5cc
Emanuelcs10 <120072706+Emanuelcs10@users.noreply.github.com>
2025-09-09 18:34:34 -0300

    Delete producao-calcados directory

--- producao-calcados/api/programa.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "GET") {
-    res.status(200).json({ status: "GET funcionando 🚀" });
-  } else if (req.method === "POST") {
-    res.status(200).json({ status: "POST funcionando 🚀" });
-  } else {
-    res.status(405).json({ error: "Método não permitido" });
-  }
-}
